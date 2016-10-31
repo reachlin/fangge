e731e9098791af3c0de88ed0f130e1bdf4a693b8
CaiLin <cailin@CaiLindeMac-mini.local>
2016-10-31 19:40:40 +0800

    stop cmd

--- LOG.md
+++ LOG.md
@@ -20,7 +20,30 @@ sudo dd bs=1m if=RuneAudio_rpi2_rp3_0.4-beta_20160321_2GB.img of=/dev/disk2
 
 ## 10/28/2016
 ```
-def JB000 Kiss.mp3
-music JB000
+def JB000 Kiss.mp3 # set the current song of JB000
+music JB000 # list the song of JB000
+# send JB000 song list to the server
 curl --data "jukebox=JB000&songs=Kiss.mp3;ItsMyLife.mp3" http://ddoer.mybluemix.net/music
+```
+
+## 10/30/2016
+* [ALSA basics](http://www.linuxjournal.com/node/6735/print)
+* [ALSA configure](http://blog.scphillips.com/posts/2013/01/sound-configuration-on-raspberry-pi-with-alsa/)
+```
+aplay -l   # find usb card number e.g. 1
+speaker-test -Dhw:1,0 -c2 -twav
+```
+
+sample alsa configure file
+```
+ls -l /usr/share/alsa/alsa.conf.d/
+~/.asoundrc  # for a user
+
+pcm.!default  {
+ type hw card 1
+}
+ctl.!default {
+ type hw card 1
+}
+
 ```
--- agent/fangge-agent.py
+++ agent/fangge-agent.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# add the next line to /etc/rc.local
+# cd /home/pi/;./fangge-agent.py
+
 import time
 import subprocess
 import urllib
@@ -11,7 +14,7 @@ DELAY = 7
 PLAYLIST = "test.m3u"
 
 def register_music():
-    onlyfiles = [f for f in listdir("Music") if isfile(join("Music", f))]
+    onlyfiles = ["%d %s" % (i+1, f) for i, f in enumerate(listdir("Music")) if isfile(join("Music", f))]
     print("%s" % onlyfiles)
     url = 'http://ddoer.mybluemix.net/music'
     values = {
@@ -31,13 +34,16 @@ def job():
     song = urllib2.urlopen("http://ddoer.mybluemix.net/music?key=JB000").read()
     if song:
         print("..."+song)
-        song = "Music/"+song
-        play_cmd = ["omxplayer", "-o", "local", song];
-        subprocess.call(play_cmd)
+        if song == "#STOP":
+            print("stop")
+        else:
+            song = "Music/"+song
+            play_cmd = ["omxplayer", "-o", "alsa", song];
+            subprocess.call(play_cmd)
     else:
         print("no song found.")
 
-
+time.sleep(7)    # without this rc.local will fail!
 register_music()
 while True:
     job()
