9941aca0174894c23c51682f2f111f70443f46d5
CaiLin <cailin@CaiLindeMac-mini.local>
2016-11-01 22:49:05 +0800

    docker

--- agent/fangge-agent.py
+++ agent/fangge-agent.py
@@ -7,12 +7,22 @@ import time
 import subprocess
 import urllib
 import urllib2
+import re
+import argparse
 from os import listdir
 from os.path import isfile, join
+
 
 JB_ID = "JB000"
 DELAY = 7
 PLAYLIST = "test.m3u"
+song_list = []
+play_cmd = "omxplayer -o alsa %s"
+
+def init():
+    global play_cmd
+    if isfile("/Applications/VLC.app/Contents/MacOS/VLC"):
+        play_cmd = "/Applications/VLC.app/Contents/MacOS/VLC --play-and-exit -I dummy %s"
 
 def get_jb_info():
     ip = ""
@@ -23,7 +33,9 @@ def get_jb_info():
     return ip
 
 def register_music():
-    onlyfiles = ["%d %s" % (i+1, f) for i, f in enumerate(listdir("Music")) if isfile(join("Music", f))]
+    global song_list
+    song_list = listdir("Music")
+    onlyfiles = ["%d %s" % (i+1, f) for i, f in enumerate(song_list) if isfile(join("Music", f))]
     print("%s" % onlyfiles)
     url = 'http://ddoer.mybluemix.net/music'
     values = {
@@ -40,20 +52,38 @@ def register_music():
 def job():
     print("%s play music..." % time.strftime("%Y/%m/%d-%H:%M:%S"))
     # Mac OS
-    #play_cmd = ["/Applications/VLC.app/Contents/MacOS/VLC", "--play-and-exit", "-I", "dummy", PLAYLIST]
+    #play_cmd = 
     song = urllib2.urlopen("http://ddoer.mybluemix.net/music?key=%s" % JB_ID).read()
     if song:
-        print("..."+song)
+        print("...play song: "+song)
+        play_song = song
         if song == "#STOP":
             print("stop")
         else:
-            song = "Music/"+song
-            play_cmd = ["omxplayer", "-o", "alsa", song];
-            subprocess.call(play_cmd)
+            try:
+                p = re.compile(r'^\d+')
+                if p.match(song):
+                    song = song_list[int(song)-1]
+                song = "Music/"+song
+                play_song = play_cmd % song
+                subprocess.call(play_song.split(" "))
+            except:
+                print("play error: %s" % play_song)
     else:
         print("no song found.")
 
+
+# main
+parser = argparse.ArgumentParser(description='play songs under a folder')
+parser.add_argument('--id', dest='id', required='true',
+                   help='id of this agent')
+
+args = parser.parse_args()
+JB_ID = args.id
+print "ID: %s" % JB_ID
+
 time.sleep(7)    # without this rc.local will fail!
+init()
 register_music()
 while True:
     job()
